3cab3c3546df78e27c90591f2169545788a1d701
qarlosalberto <carlosruiznaranjo@gmail.com>
2023-04-05 12:38:13 +0200

    Introduction

--- docs/01-introduction.md
+++ docs/01-introduction.md
@@ -4,3 +4,19 @@ id: intro
 ---
 
 # Introduction
+
+<p align="center">
+
+![TerosHDL header](/img/bcard-front-low.png) 
+</p>
+
+
+The goal of TerosHDL is to provide an **open source IDE** for HDL devlopers with functionalities commonly used by software developers. The IDE consist in a bunch of tools and on top of them is **VSCodium**/VSCode. 
+Some tools are developed by Teros Technology organization and others come from open source projects. All the tools are organized in different backends and exposed to the GUI with the plugin.
+
+The IDE tries to be as much self-contained as possible and simplify the installation process.
+
+<p align="center">
+
+![TerosHDL in action](/img/super.gif) 
+</p>
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -14,28 +14,29 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        TerosHDL has a simple and intuitive interface, with helpful features such as code completion, 
+        templates, and error checker that make it easy to use.
       </>
     ),
   },
   {
     title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        TerosHDL helps in code verification by providing 
+        features such as syntax highlighting, 
+        and auto documentation tools that help identify and fix errors in the code.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Open source',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        TerosHDL is freely available for use, modification, and distribution by anyone. 
+        This promotes accessibility and reduces costs for HDL designers.
       </>
     ),
   },
